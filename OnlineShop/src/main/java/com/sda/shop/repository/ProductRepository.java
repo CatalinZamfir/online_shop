544a54b14d30466135961ca80e3060844d8e4550
Zamfir Catalin <catalin_provideo@yahoo.com>
2020-11-22 16:30:26 +0200

    commit

--- OnlineShop/src/main/java/com/sda/shop/repository/ProductRepository.java
+++ OnlineShop/src/main/java/com/sda/shop/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.sda.shop.repository;
 
 import com.sda.shop.entity.ProductCategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductCategoryEntity, Integer> {
 }
